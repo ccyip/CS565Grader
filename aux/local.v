53c770d3d9c6bfbf8415da34344580297cb66f05
Qianchuan Ye <yeqianchuan@gmail.com>
2020-12-09 15:51:49 -0500

    Update Coq scripts.

--- aux/local.v
+++ aux/local.v
@@ -5,3 +5,8 @@ Definition Comment := tt.
 Definition Penalty := tt.
 
 (* Manual grade items *)
+Definition mg_exc1 := tt.
+Definition mg_exc2 := tt.
+Definition mg_exc3 := tt.
+Definition mg_exc4 := tt.
+Definition mg_exc5 := tt.
